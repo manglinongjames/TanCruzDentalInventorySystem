d28628d87f3612770a7293e0c15180efa797928f
erwin-velasquez <50427894+erwin-velasquez@users.noreply.github.com>
2019-06-01 15:23:14 +0800

    Initial cleanup of IdentityManager

--- IdentityManagement/Entities/UserInfo.cs
+++ IdentityManagement/Entities/UserInfo.cs
@@ -1,10 +1,5 @@
 ﻿using IdentityManagement.Utilities;
 using Microsoft.AspNet.Identity;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace IdentityManagement.Entities
 {
--- IdentityManagement/IdentityStore/UserStore.cs
+++ IdentityManagement/IdentityStore/UserStore.cs
@@ -3,8 +3,6 @@ using IdentityManagement.Entities;
 using Microsoft.AspNet.Identity;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace IdentityManagement.IdentityStore
